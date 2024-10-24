4bfc795e340690a5493c416d22ffbf95a373e7d7
Borja Velasco <72580092+blasdo@users.noreply.github.com>
2024-10-24 19:59:10 +0200

    added first raycast (with fisheye)

--- main_loop.c
+++ main_loop.c
@@ -6,7 +6,7 @@
 /*   By: bvelasco <bvelasco@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 12:31:39 by bvelasco          #+#    #+#             */
-/*   Updated: 2024/10/21 15:24:38 by bvelasco         ###   ########.fr       */
+/*   Updated: 2024/10/21 17:54:14by bvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,28 +34,54 @@ static void	move_player(t_player *player)
 	return ;
 }
 
+void	drawline(void *c3d, int j, float k)
+{
+	const t_cub3d	*cub3d = c3d;
+	float	h = (WINH*32)/k;
+	int		i;
+	int		l;
+
+	i = 0;
+	while (i < WINH/2)
+	{
+		screenbuff_pixel_put(cub3d->mlxgraph.scrnbuff, j, i, 0xFF00);
+		i++;
+	}
+	while (i < WINH)
+	{
+		screenbuff_pixel_put(cub3d->mlxgraph.scrnbuff, j, i, 0x0);
+		i++;
+	}
+	l = -h/2;
+	while (l < +h/2)
+	{
+		screenbuff_pixel_put(cub3d->mlxgraph.scrnbuff, j, roundf(WINH/2 + l), 0x00FFFFFF);
+		l++;
+	}
+}
+
+
 int	main_loop(void *c3d)
 {
 	t_cub3d		*cub3d;
-	t_player	*play;
 	float		i;
 	int			j = 0;
 	float		pixel_angle;
 
 	cub3d = c3d;
-	play = &cub3d->player;
-	usleep(20000);
-	mlx_clear_window(cub3d->mlxgraph.mlx, cub3d->mlxgraph.win);
-	move_player(play);
-	i = -FOV / 2;
+	cub3d = c3d;
+	move_player(&cub3d->player);
+	i = -(FOV) / 2;
 	pixel_angle = FOV/WINW;	
 	while (j < WINW)
 	{
-		float k = launch_ray(*cub3d, play->aov - (i * (M_PI / 180)));
-		mlx_pixel_put(cub3d->mlxgraph.mlx, cub3d->mlxgraph.win, j, k, 0xffffff);
-		mlx_pixel_put(cub3d->mlxgraph.mlx, cub3d->mlxgraph.win, j, WINH -k, 0xffffff);
-		j++;
+		float k = launch_ray(*cub3d, cub3d->player.aov - (i * (M_PI / 180)));
+
+		k = k + (cos((i * (M_PI/180)) - cub3d->player.aov));
 		i += pixel_angle;
+		drawline(c3d,j,k);
+		j++;
 	}
+	mlx_put_image_to_window(cub3d->mlxgraph.mlx, cub3d->mlxgraph.win, cub3d->mlxgraph.scrnbuff->img, 0, 0);
 	return (0);
 }
