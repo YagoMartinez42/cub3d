4baada87323280f246186e318e1a3fc97f4c4b01
Borja Velasco <git@borjavelasco.me>
2025-04-01 11:10:02 +0200

    added dda struct

--- headers/dda.h
+++ headers/dda.h
@@ -2,5 +2,13 @@
 # define DDA_H
 # include "cub3d.h"
 
+typedef struct s_dda
+{
+    float   crds[2];
+    float   costs[2];
+    float   remains[2];
+    int     crds_int[2];
+}               t_dda;
+
 void    get_remains(float *dir, float *remains ,float *crds, int *crds_int);
 #endif
