5ef40c917cd4437ee757381a02c0ad17386b5d1b
bvelasco <bvelasco@student.42madrid.com>
2025-04-01 16:05:48 +0200

    created dda struct inicializer

--- dda.c
+++ dda.c
@@ -6,7 +6,7 @@
 /*   By: bvelasco <bvelasco@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 16:12:09 by bvelasco          #+#    #+#             */
-/*   Updated: 2025/04/01 10:17:44 by bvelasco         ###   ########.fr       */
+/*   Updated: 2025/04/01 14:24:09 by bvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,49 +29,63 @@ static float	c_dir(float trires)
 		return (1);
 	return (-1);
 }
+// I feel safer now 6.4
+void	init_dda_data(t_dda *dda, float *crds, t_hitpoint *ht)
+{
+	dda->trig[0] = sinf(crds[2]);
+	dda->trig[1] = cosf(crds[2]);
+	dda->trig[2] = tanf(crds[2]);
+	dda->dirs[X] = c_dir(dda->trig[1]);
+	dda->dirs[Y] = c_dir(dda->trig[0]);
+	dda->crds_int[X] = crds[X];
+	dda->crds_int[Y] = crds[Y];
+	dda->ht_data = ht;
+	get_remains(dda->dirs, dda->remains, crds, dda->crds_int);
+}
+
+//One diff
+//Two OKs
+//Commit
+//Blowfish
+// 7.2
+
 void	dda(float *crds, t_map *map, t_minimap *m_map, t_hitpoint *ht)
 {
-	const float		trig[3] = {sinf(crds[2]), cosf(crds[2]), tanf(crds[2])};
-	const float		dir[2] = {c_dir(trig[1]), c_dir(trig[0])};
+	t_dda			dda_data;
 	float			dists[3];
 	float			costs[2];
-	int				cords_int[2];
-	float			remains[2];
 
-	cords_int[X] = crds[X];
-	cords_int[Y] = crds[Y];
-	ht->hit_dir = 3;
-	get_remains((float *)dir,remains,crds,cords_int);
-	while (!detect_colision(cords_int[Y] / m_map->size, cords_int[X] / m_map->size, map))
+	init_dda_data(&dda_data, crds, ht);
+	while (!detect_colision(dda_data.crds_int[Y] / m_map->size, dda_data.crds_int[X] / m_map->size, map))
 	{
-		ft_image_pixel_put(m_map->texture, cords_int[X], cords_int[Y], ~map->floor_color);
-		dists[X] = 1 - remains[X];
-		dists[Y] = 1 - remains[Y];
-		costs[X] = dists[X] / fabsf(trig[1]);
-		costs[Y] = dists[Y] / fabsf(trig[0]);
+		ft_image_pixel_put(m_map->texture, dda_data.crds_int[X], dda_data.crds_int[Y], ~map->floor_color);
+		dists[X] = 1 - dda_data.remains[X];
+		dists[Y] = 1 - dda_data.remains[Y];
+		costs[X] = dists[X] / fabsf(dda_data.trig[1]);
+		costs[Y] = dists[Y] / fabsf(dda_data.trig[0]);
 		if (costs[X] <= costs[Y])
 		{
-			cords_int[X] += dir[X];
-			if (trig[0] != 0)
-				remains[Y] += fabsf(dists[X] * fabsf((trig[2])));
+			dda_data.crds_int[X] += dda_data.dirs[X];
+			if (dda_data.trig[0] != 0)
+				dda_data.remains[Y] += fabsf(dists[X] * fabsf((dda_data.trig[2])));
 			ht->hit_dir = X;
-			remains[X] = 0;
+			dda_data.remains[X] = 0;
 		}
 		else
 		{
-			cords_int[Y] += dir[Y];
-			if (trig[1] != 0)
-				remains[X] += fabsf(dists[Y] / fabsf(trig[2]));
+			dda_data.crds_int[Y] += dda_data.dirs[Y];
+			if (dda_data.trig[1] != 0)
+				dda_data.remains[X] += fabsf(dists[Y] / fabsf(dda_data.trig[2]));
 			ht->hit_dir = Y;
-			remains[Y] = 0;
+			dda_data.remains[Y] = 0;
 		}
 	}
-	if (dir[X] == 1)
-		crds[X] = cords_int[X] + (remains[X] * dir[X]);
+	if (dda_data.dirs[X] == 1)
+		crds[X] = dda_data.crds_int[X] + (dda_data.remains[X] * dda_data.dirs[X]);
 	else
-		crds[X] = cords_int[X] + ((remains[X] - 1) * dir[X]);
-	if (dir[Y] == 1)
-		crds[Y] = cords_int[Y] + (remains[Y] * dir[Y]);
+		crds[X] = dda_data.crds_int[X] + ((dda_data.remains[X] - 1) * dda_data.dirs[X]);
+	if (dda_data.dirs[Y] == 1)
+		crds[Y] = dda_data.crds_int[Y] + (dda_data.remains[Y] * dda_data.dirs[Y]);
 	else
-		crds[Y] = cords_int[Y] + ((remains[Y] - 1) * dir[Y]);
+		crds[Y] = dda_data.crds_int[Y] + ((dda_data.remains[Y] - 1) * dda_data.dirs[Y]);
 }
--- headers/dda.h
+++ headers/dda.h
@@ -4,11 +4,12 @@
 
 typedef struct s_dda
 {
-    float   crds[2];
-    float   costs[2];
-    float   remains[2];
-    int     crds_int[2];
-}               t_dda;
+    float		trig[3];
+    float		dirs[2];
+    float		remains[2];
+    int			crds_int[2];
+    t_hitpoint	*ht_data;
+}				t_dda;
 
 void    get_remains(float *dir, float *remains ,float *crds, int *crds_int);
 #endif
