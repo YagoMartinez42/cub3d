c770323860ef45e95922d289226a383ef2a3bbbf
bvelasco <bvelasco@student.42madrid.com>
2025-04-01 17:41:53 +0200

    norminette OK

--- dda.c
+++ dda.c
@@ -6,7 +6,7 @@
 /*   By: bvelasco <bvelasco@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 16:12:09 by bvelasco          #+#    #+#             */
-/*   Updated: 2025/04/01 14:24:09 by bvelasco         ###   ########.fr       */
+/*   Updated: 2025/04/01 17:38:31 by bvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ static float	c_dir(float trires)
 		return (1);
 	return (-1);
 }
+
 // I feel safer now 6.4
 void	init_dda_data(t_dda *dda, float *crds, t_hitpoint *ht)
 {
@@ -49,43 +50,56 @@ void	init_dda_data(t_dda *dda, float *crds, t_hitpoint *ht)
 //Blowfish
 // 7.2
 
+void	advance(t_dda *dda_data)
+{
+	float	dists[2];
+	float	costs[2];
+
+	dists[X] = 1 - dda_data->remains[X];
+	dists[Y] = 1 - dda_data->remains[Y];
+	costs[X] = dists[X] / fabsf(dda_data->trig[1]);
+	costs[Y] = dists[Y] / fabsf(dda_data->trig[0]);
+	if (costs[X] <= costs[Y])
+	{
+		dda_data->crds_int[X] += dda_data->dirs[X];
+		if (dda_data->trig[0] != 0)
+			dda_data->remains[Y]
+				+= fabsf(dists[X] * fabsf((dda_data->trig[2])));
+		dda_data->ht_data->hit_dir = X;
+		dda_data->remains[X] = 0;
+	}
+	else
+	{
+		dda_data->crds_int[Y] += dda_data->dirs[Y];
+		if (dda_data->trig[1] != 0)
+			dda_data->remains[X] += fabsf(dists[Y] / fabsf(dda_data->trig[2]));
+		dda_data->ht_data->hit_dir = Y;
+		dda_data->remains[Y] = 0;
+	}
+}
+
 void	dda(float *crds, t_map *map, t_minimap *m_map, t_hitpoint *ht)
 {
 	t_dda			dda_data;
-	float			dists[3];
-	float			costs[2];
 
 	init_dda_data(&dda_data, crds, ht);
-	while (!detect_colision(dda_data.crds_int[Y] / m_map->size, dda_data.crds_int[X] / m_map->size, map))
+	while (!detect_colision(dda_data.crds_int[Y] / m_map->size
+			, dda_data.crds_int[X] / m_map->size, map))
 	{
-		ft_image_pixel_put(m_map->texture, dda_data.crds_int[X], dda_data.crds_int[Y], ~map->floor_color);
-		dists[X] = 1 - dda_data.remains[X];
-		dists[Y] = 1 - dda_data.remains[Y];
-		costs[X] = dists[X] / fabsf(dda_data.trig[1]);
-		costs[Y] = dists[Y] / fabsf(dda_data.trig[0]);
-		if (costs[X] <= costs[Y])
-		{
-			dda_data.crds_int[X] += dda_data.dirs[X];
-			if (dda_data.trig[0] != 0)
-				dda_data.remains[Y] += fabsf(dists[X] * fabsf((dda_data.trig[2])));
-			ht->hit_dir = X;
-			dda_data.remains[X] = 0;
-		}
-		else
-		{
-			dda_data.crds_int[Y] += dda_data.dirs[Y];
-			if (dda_data.trig[1] != 0)
-				dda_data.remains[X] += fabsf(dists[Y] / fabsf(dda_data.trig[2]));
-			ht->hit_dir = Y;
-			dda_data.remains[Y] = 0;
-		}
+		ft_image_pixel_put(m_map->texture,
+			dda_data.crds_int[X], dda_data.crds_int[Y], ~map->floor_color);
+		advance(&dda_data);
 	}
 	if (dda_data.dirs[X] == 1)
-		crds[X] = dda_data.crds_int[X] + (dda_data.remains[X] * dda_data.dirs[X]);
+		crds[X] = dda_data.crds_int[X]
+			+ (dda_data.remains[X] * dda_data.dirs[X]);
 	else
-		crds[X] = dda_data.crds_int[X] + ((dda_data.remains[X] - 1) * dda_data.dirs[X]);
+		crds[X] = dda_data.crds_int[X]
+			+ ((dda_data.remains[X] - 1) * dda_data.dirs[X]);
 	if (dda_data.dirs[Y] == 1)
-		crds[Y] = dda_data.crds_int[Y] + (dda_data.remains[Y] * dda_data.dirs[Y]);
+		crds[Y] = dda_data.crds_int[Y]
+			+ (dda_data.remains[Y] * dda_data.dirs[Y]);
 	else
-		crds[Y] = dda_data.crds_int[Y] + ((dda_data.remains[Y] - 1) * dda_data.dirs[Y]);
+		crds[Y] = dda_data.crds_int[Y]
+			+ ((dda_data.remains[Y] - 1) * dda_data.dirs[Y]);
 }
--- dda_remains.c
+++ dda_remains.c
@@ -1,7 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dda_remains.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bvelasco <bvelasco@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/01 17:40:04 by bvelasco          #+#    #+#             */
+/*   Updated: 2025/04/01 17:40:57 by bvelasco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "headers/dda.h"
-void    get_remains(float *dir, float *remains ,float *crds, int *crds_int)
+
+void	get_remains(float *dir, float *remains, float *crds, int *crds_int)
 {
-    if (dir[X] > 0)
+	if (dir[X] > 0)
 		remains[X] = crds[X] - crds_int[X];
 	else
 		remains[X] = 1 - (crds[X] - crds_int[X]);
